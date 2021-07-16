a9a916ba5e4fa3579060c2db1e81d032b7e04863
williamchavula <rumbani@gmail.com>
2021-07-15 21:01:22 -0500

    * PhotoImporter.cs: implement IPhotoImporter class in Android   Project. This class handles logic for importing photos after user   has granted permission.
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * Entitlements.plist:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * photoGalleryApp.iOS.csproj:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * Icon29.png:
    * Icon20.png:
    * Icon76.png:
    * Icon80.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon87.png:
    * Icon180.png:
    * Icon152.png:
    * Icon120.png:
    * Icon167.png:
    * Icon1024.png:
    * Contents.json: Removed the iOS project. Not developing for iOS.
    
    * photoGalleryApp.sln:
    * photoGalleryApp.csproj: specify C# version 8.0 for development
    
    * MainActivity.cs: Add logid to call method when we get resultCode in
      onRequestPermissionsResult
    
    * customview-map.txt:
    * photoGalleryApp.Android.csproj:
    * Resource.designer.cs:
    * build.props:
    * libraryimports.cache:
    * Resource.designer.cs:
    * libraryprojectimports.cache:

--- photoGalleryApp.Android/MainActivity.cs
+++ photoGalleryApp.Android/MainActivity.cs
@@ -10,9 +10,11 @@ namespace photoGalleryApp.Droid
     [Activity(Label = "photoGalleryApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static MainActivity Current { get; private set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            Current = this;
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -21,6 +23,12 @@ namespace photoGalleryApp.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == 33)
+            {
+                //var importer = (PhotoImporter)Resolver.Resolve<IPhotoImporter>();
+                // importer.ContinueWithPermission(grantResults[0] == Permission.Granted);
+            }
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
