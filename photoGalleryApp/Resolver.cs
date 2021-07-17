49d198ebccf18cea2ff4311dae0477d0bcb4fe63
williamchavula <rumbani@gmail.com>
2021-07-16 21:25:12 -0500

    * Bootstrapper.cs: Register the PhotoImporter to the IOC Container.   It should be instantiated when IPhotoImporter is requested
    * Resolver.cs:
    * Bootstrapper.cs: Add AutoFac for IOC and register the types in the
      app.
    
    * MainShell.xaml:
    * MainShell.xaml.cs: Add Shell app
    
    * MainActivity.cs: add permission check for photo access. and invoke
      ContinueWithPermission method
    
    * photoGalleryApp.Android.csproj:
    * build.props: add AutoFac Nuguet Package
    
    * PhotoImporter.cs: Add [Obsolete] flag to methods in PhotoImporter in
      order to work with Data column of MediaStore.Images.ImageColumns
      table.
    
    * App.xaml.cs: Change MainPage to MainShell();
    
    * photoGalleryApp.csproj: Install AutoFac, and add ViewModel abstract
      class, and its implementation GalleryViewModel class in ViewModels
      directory. Add View classes in Views directory

---
+++ photoGalleryApp/Resolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autofac;
+
+namespace photoGalleryApp
+{
+    public class Resolver
+    {
+
+        private static IContainer _container;
+        public Resolver()
+        {
+        }
+
+        public static void Initialize(IContainer container)
+        {
+            _container = container;
+        }
+
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}
